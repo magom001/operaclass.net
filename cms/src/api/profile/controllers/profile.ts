26e6e04da54c8d9b240bd55a286d33c63cbaad35
Arkady Magomedov <amagomedov@microsoft.com>
2024-03-22 16:34:44 +0100

    Switch from Pianist to Profile

--- cms/src/api/profile/controllers/profile.ts
+++ cms/src/api/profile/controllers/profile.ts
@@ -2,6 +2,25 @@
  * profile controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::profile.profile');
+export default factories.createCoreController(
+  "api::profile.profile",
+  ({ strapi }) => ({
+    async findBySlug(ctx) {
+      await this.validateQuery(ctx);
+      const sanitizedQueryParams = await this.sanitizeQuery(ctx);
+      const { locale } = sanitizedQueryParams;
+      const { slug } = ctx.params;
+
+      const entity = await strapi.db.query("api::profile.profile").findOne({
+        where: { slug, locale },
+        populate: true,
+      });
+
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
--- frontend/src/services/pianists.ts
+++ frontend/src/services/pianists.ts
@@ -1,7 +1,14 @@
 import type { Locale } from "@/i18n";
 import qs from "qs";
 import { host, token } from "./config";
-import { Block, Response, ProfileType, MetaData, VideoLinkType } from "./types";
+import {
+  Block,
+  Response,
+  ProfileType,
+  MetaData,
+  VideoLinkType,
+  ContactInfoType,
+} from "./types";
 
 export interface PianistPreview {
   id: number;
@@ -42,7 +49,7 @@ export interface Pagination {
   page?: number;
 }
 
-export async function getPianistsPreviewV2(
+export async function getPianistsPreview(
   locale: Locale,
   searchParams: SearchParams,
   pagination: Pagination = {}
@@ -163,188 +170,6 @@ export async function getPianistsPreviewV2(
   return { data: transformed, meta };
 }
 
-export async function getPianistsPreview(
-  locale: Locale,
-  searchParams: SearchParams,
-  pagination: Pagination = {}
-): Promise<{ data: PianistPreview[]; meta: MetaData }> {
-  const {
-    city,
-    speaks = [],
-    reads = [],
-    experience = [],
-    goal = [],
-  } = searchParams;
-  const { page = 1 } = pagination;
-
-  const spokenLanguagesFilter = (Array.isArray(speaks) ? speaks : [speaks]).map(
-    (alpha2) => ({
-      speaks: {
-        alpha2: {
-          $eq: alpha2,
-        },
-      },
-    })
-  );
-
-  const readLanguagesFilter = (Array.isArray(reads) ? reads : [reads]).map(
-    (alpha2) => ({
-      reads: {
-        alpha2: {
-          $eq: alpha2,
-        },
-      },
-    })
-  );
-
-  const goalFilter = (Array.isArray(goal) ? goal : [goal]).map((code) => ({
-    goals: {
-      code: {
-        $eq: code,
-      },
-    },
-  }));
-
-  const experiencesFilter = (
-    Array.isArray(experience) ? experience : [experience]
-  ).map((code) => ({
-    experiences: {
-      code: {
-        $eq: code,
-      },
-    },
-  }));
-
-  const query = qs.stringify(
-    {
-      pagination: {
-        page,
-      },
-      fields: ["fullName", "sex", "slug"],
-      populate: {
-        city: {
-          fields: ["name", "code"],
-        },
-        previewVideo: {
-          fields: ["url"],
-        },
-      },
-      locale,
-      sort: ["rating:desc"],
-      filters: {
-        city: {
-          code: {
-            $in: city,
-          },
-        },
-        $and: [
-          ...spokenLanguagesFilter,
-          ...readLanguagesFilter,
-          ...experiencesFilter,
-          ...goalFilter,
-        ],
-      },
-    },
-    {
-      encodeValuesOnly: true,
-    }
-  );
-
-  const response = await fetch(`${host}/api/pianists?${query}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      Accept: "application/json",
-    },
-  });
-
-  const result: Response<PianistsPreviewResponseData> = await response.json();
-
-  const { meta, data } = result;
-
-  const transformed = data.map((p) => {
-    const preview = p.attributes.previewVideo;
-
-    return {
-      id: p.id,
-      fullName: p.attributes.fullName,
-      slug: p.attributes.slug,
-      city: p.attributes.city?.data.attributes.name,
-      sex: p.attributes.sex,
-      previewVideo: preview,
-    } satisfies PianistPreview;
-  });
-
-  return { data: transformed, meta };
-}
-
-interface PianistResponseData {
-  id: number;
-  attributes: {
-    fullName: string;
-    sex: "male" | "female";
-    slug: string;
-    previewVideo: null | { url: string };
-    bio: Block[];
-    city: {
-      data: {
-        attributes: {
-          name: string;
-        };
-      };
-    };
-    reads: {
-      data: {
-        attributes: {
-          name: string;
-        };
-      }[];
-    };
-    speaks: {
-      data: {
-        attributes: {
-          name: string;
-        };
-      }[];
-    };
-    goals: {
-      data: {
-        attributes: {
-          name: string;
-        };
-      }[];
-    };
-    experiences: {
-      data: {
-        attributes: {
-          name: string;
-        };
-      }[];
-    };
-    recommendations: {
-      name: string;
-      profileLink: string;
-    }[];
-    contacts: {
-      type: ContactInfo["type"];
-      data: string;
-    }[];
-  };
-}
-
-export interface ContactInfo {
-  type:
-    | "email"
-    | "phone"
-    | "whatsapp"
-    | "telegram"
-    | "facebook"
-    | "instagram"
-    | "linkedin"
-    | "url"
-    | "vk";
-  data: string;
-}
-
 interface Recommendation {
   name: string;
   profileLink: string;
@@ -362,69 +187,7 @@ interface Pianist {
   previewVideo: null | {
     url: string;
   };
-  contacts?: ContactInfo[];
-}
-
-export async function getPianistById(
-  locale: Locale,
-  id: string
-): Promise<Pianist | null> {
-  const query = qs.stringify({
-    fields: ["fullName", "sex", "bio"],
-    locale,
-    populate: {
-      previewVideo: {
-        fields: ["url"],
-      },
-      contacts: {
-        fields: ["type", "data"],
-      },
-      city: {
-        fields: ["name"],
-      },
-      reads: {
-        fields: ["name"],
-      },
-      speaks: {
-        fields: ["name"],
-      },
-      goals: {
-        fields: ["name"],
-      },
-      experiences: {
-        fields: ["name"],
-      },
-      recommendations: {
-        fields: ["name", "profileLink"],
-      },
-    },
-  });
-
-  const response = await fetch(`${host}/api/pianists/${id}?${query}`, {
-    headers: { Authorization: `Bearer ${token}`, Accept: "application/json" },
-  });
-
-  const { data }: { data: PianistResponseData | null } = await response.json();
-
-  if (!data) {
-    return null;
-  }
-
-  return {
-    fullName: data.attributes.fullName,
-    city: data.attributes.city.data.attributes.name,
-    bio: data.attributes.bio,
-    reads: data.attributes.reads.data.map((l) => l.attributes.name),
-    speaks: data.attributes.speaks.data.map((l) => l.attributes.name),
-    goals: data.attributes.goals.data.map((l) => l.attributes.name),
-    experiences: data.attributes.experiences.data.map((l) => l.attributes.name),
-    previewVideo: data.attributes.previewVideo,
-    recommendations: data.attributes.recommendations,
-    contacts: data.attributes.contacts?.map((c) => ({
-      type: c.type,
-      data: c.data,
-    })),
-  };
+  contacts?: ContactInfoType[];
 }
 
 export async function getPianistBySlug(
@@ -434,25 +197,29 @@ export async function getPianistBySlug(
   const query = qs.stringify({
     locale,
   });
-  const response = await fetch(`${host}/api/pianists/slug/${slug}?${query}`, {
+  const response = await fetch(`${host}/api/profiles/slug/${slug}?${query}`, {
     headers: { Authorization: `Bearer ${token}`, Accept: "application/json" },
   });
 
-  const { data }: { data: PianistResponseData | null } = await response.json();
+  const { data }: { data: ProfileType | null } = await response.json();
 
   if (!data) {
     return null;
   }
 
   return {
-    fullName: data.attributes.fullName,
-    city: data.attributes.city.data.attributes.name,
+    fullName: data.attributes.firstName + " " + data.attributes.lastName,
+    city: data.attributes.city?.data.attributes.name,
     bio: data.attributes.bio,
-    reads: data.attributes.reads.data.map((l) => l.attributes.name),
-    speaks: data.attributes.speaks.data.map((l) => l.attributes.name),
-    goals: data.attributes.goals.data.map((l) => l.attributes.name),
-    experiences: data.attributes.experiences.data.map((l) => l.attributes.name),
-    previewVideo: data.attributes.previewVideo,
+    reads: data.attributes.reads?.data.map((l) => l.attributes.name),
+    speaks: data.attributes.speaks?.data.map((l) => l.attributes.name),
+    goals: data.attributes.goals?.data.map((l) => l.attributes.name),
+    experiences: data.attributes.experiences?.data.map(
+      (l) => l.attributes.name
+    ),
+    previewVideo: data.attributes.videos?.length
+      ? data.attributes.videos[0]
+      : null,
     recommendations: data.attributes.recommendations,
     contacts: data.attributes.contacts?.map((c) => ({
       type: c.type,
